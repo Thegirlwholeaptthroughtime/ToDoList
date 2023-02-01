c7cec937791bf72a50bb3699463c63c5733a4328
Thegirlwholeaptthroughtime <kdh516094@gmail.com>
2023-02-01 21:10:25 +0900

    delete todo

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+
+
+}
+
 function paintToDo(newTodo) {
   console.log(newTodo);
   const li = document.createElement("li");
-  const span = document.createElement('span')
-  li.appendChild(span)
-  span.innerText = (newTodo)
-  toDoList.appendChild(li)
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+
+  span.innerText = newTodo;
+
+  li.appendChild(span);
+  li.appendChild(button);
+
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(e) {
